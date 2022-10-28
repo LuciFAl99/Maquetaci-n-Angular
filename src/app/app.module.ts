75369523cb980d639ca4d16abb91d312925a1164
LuciFAl99 <lucila.alochis1999@gmail.com>
2022-10-28 20:59:43 -0300

    subiendo los primeros archivos

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    CarruselComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    PieDePaginaComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
