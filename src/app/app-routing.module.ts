78a4da6d596be44f92fd3b76f6e9980b7a69694f
LuciFAl99 <lucila.alochis1999@gmail.com>
2022-11-14 21:03:10 -0300

    creación de rutas hacia componentes. form login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'HeaderComponent' , pathMatch: 'full' },
+  { path: 'acerca-de', component: AcercaDeComponent},
+  { path: 'educacion', component: EducacionComponent},
+  { path: 'experiencia', component: ExperienciaComponent},
+  { path: 'habilidades', component: HabilidadesComponent},
+  { path: 'proyectos', component: ProyectosComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'carrusel', component: CarruselComponent }
+]
+  
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
@@ -12,6 +11,8 @@ import { EducacionComponent } from './componentes/educacion/educacion.component'
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
+import { LoginComponent } from './componentes/login/login.component';
+
 
 
 
@@ -26,11 +27,18 @@ import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.
     ProyectosComponent,
     HabilidadesComponent,
     PieDePaginaComponent,
+    LoginComponent,
+    
+
+  
+
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
